85d90ec9ad0caa75926f0c99ecc654c9e2fb4fc9
Danny Arntz <arntz.D@gmail.com>
2019-04-01 20:54:49 +0200

    first commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,15 @@
+import 'swiper/dist/css/swiper.css';
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+import Swiper from 'swiper/dist/js/swiper.min';
+
+const mySwiper = new Swiper('.swiper-container', {
+  speed: 400,
+  spaceBetween: 100,
+  slidesPerView: 1,
+  effect: 'fade',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
